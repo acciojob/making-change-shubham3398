89f7b332e1c7eb438ba0cb57714916eece575467
shubham3398 <117896201+shubham3398@users.noreply.github.com>
2023-12-18 13:54:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
 const makeChange = (c) => {
   // your name here
+	let obj = {"q" : 0, "d" : 0, "n" : 0, "p" : 0};
+	
+	obj["q"] = Math.floor(c / 25);
+	c = c % 25
+	
+	obj["d"] = Math.floor(c/10);
+	c = c%10;
+
+	obj["n"] = Math.floor(c / 5);
+	c = c % 5;
+
+	obj["p"] = Math.floor(c / 1);
+
+	return obj;
 };
 
 // Do not the change the code below
 const c = prompt("Enter c: ");
-alert(JSON.stringify(makeChange(c)));
+alert(JSON.stringify(makeChange(c))); 
